76aa3b87a25c34038e8fb399533e7cf03a1af384
Cronos <main.cronos@protonmail.com>
2023-12-06 17:51:39 +0100

    st added

--- suckless-deploy.sh
+++ suckless-deploy.sh
@@ -4,7 +4,7 @@ echo "Run this script as super user"
 mkdir suckless && cd suckless
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
-
+git crone https://git.suckless.org/st
 cd dwm && make clean install
 mkdir patches && cd patches
 wget "https://dwm.suckless.org/patches/alt-tab/dwm-alttab-6.4.diff"
@@ -12,8 +12,8 @@ wget "https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-202
 wget "https://dwm.suckless.org/patches/attachbelow/dwm-attachbelow-6.2.diff"
 wget "https://dwm.suckless.org/patches/steam/dwm-steam-6.2.diff"
 cd ../ && patch -i patches/* && sudo make clean install
-cd ../
-cd dmenu && make clean install
+cd ../dmenu && make clean install
+cd ../st && make clean install
 cd ../../
 
 echo "DWM deployed \n relogin and startx"
