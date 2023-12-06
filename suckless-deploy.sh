cf0eb4a023754e29605ef9125e18238a2c83c692
Cronos <main.cronos@protonmail.com>
2023-12-06 17:34:06 +0100

    dwm install scrip

---
+++ suckless-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Run this script as super user"
+
+mkdir suckless && cd suckless
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+
+cd dwm && make clean install
+mkdir patches && cd patches
+wget "https://dwm.suckless.org/patches/alt-tab/dwm-alttab-6.4.diff"
+wget "https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff"
+wget "https://dwm.suckless.org/patches/attachbelow/dwm-attachbelow-6.2.diff"
+wget "https://dwm.suckless.org/patches/steam/dwm-steam-6.2.diff"
+cd ../ && patch -i patches/* && sudo make clean install
+cd ../
+cd dmenu && make clean install
+cd ../../
+
+echo "DWM deployed \n relogin and startx"
